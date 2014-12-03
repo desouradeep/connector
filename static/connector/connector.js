96ef035f7de25b305e688f2c8c7a705492bd7912
Souradeep De <souradeep.2011@gmail.com>
2014-12-03 20:27:43 +0530

    Add usage parameters

--- static/connector/connector.js
+++ static/connector/connector.js
@@ -17,8 +17,99 @@ $(document).ready(function() {
         console.log(data);
     });
 
+    var consumption = {};
+
     socket.on('internet_usage', function(payload) {
         payload = JSON.parse(payload);
-        console.log(payload);
+
+        var html = '';
+        var ips = Object.keys(payload).sort();
+
+        var gross_speed = get_gross_speed(payload, ips);
+        for(var i = 0 ; i < ips.length ; i++)
+        {
+            var data = [];
+            var ip = ips[i];
+
+            var details = payload[ip];
+
+            data.push(ip);
+            data.push(details['name']);
+            data.push(details['mac']);
+
+            var tcp = check_undefined_speed(details['tcp']);
+            data.push(tcp);
+
+            var udp = check_undefined_speed(details['udp']);
+            data.push(udp);
+
+            var total = tcp + udp;
+            data.push(total);
+
+
+            var percentage = total / gross_speed * 100;
+            data.push(percentage.toFixed(0) + "%");
+
+
+            if( isNaN(consumption[ip]) )
+                consumption[ip] = 0;
+
+            consumption[ip] += total;
+
+            data.push(consumption[ip]);
+
+
+            var total_consumption = get_total_consumption(consumption);
+            percentage = consumption[ip] / total_consumption * 100;
+            data.push(percentage.toFixed(0) + "%");
+
+
+            html = html.concat(draw_row(data));
+        }
+
+        $("#usage-table").html(html);
     });
 });
+
+function draw_row(data) {
+    html = '<tr>';
+    for(var i = 0; i < data.length; i++) {
+        var det = data[i];
+        if( det === undefined || det == 'NaN') {
+            det = '';
+        }
+        var td_html = "<td>" + det + "</td>";
+        html = html.concat(td_html);
+    }
+    html = html.concat('</tr>');
+    return html;
+}
+
+function check_undefined_speed(speed) {
+    if( speed === undefined) {
+        return 0;
+    }
+    else {
+        return speed;
+    }
+}
+
+function get_gross_speed(payload, ips) {
+    var gross_speed = 0;
+    for(var i = 0 ; i < ips.length ; i++) {
+        var details = payload[ips[i]];
+        gross_speed += check_undefined_speed(details['tcp']) +
+            check_undefined_speed(details['udp']);
+    }
+    return gross_speed;
+}
+
+function get_total_consumption(consumption_list) {
+    var total_consumption = 0;
+
+    var keys = Object.keys(consumption_list);
+    for( var i = 0 ; i < keys.length ; i++ ) {
+        total_consumption += consumption_list[keys[i]];
+    }
+    return total_consumption;
+}
